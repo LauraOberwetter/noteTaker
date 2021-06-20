8c65baffe06820c797b42bdaeafd9b49843fd470
Laura Oberwetter <leoberwetter@gmail.com>
2021-06-19 21:48:13 -0500

    get route started

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,3 +1,16 @@
 const fs = require("fs");
 const router = require('express').Router();
 //const store = require('../db/store'); //use this if making seperate store.js file for data
+
+//GET notes
+router.get('/notes', (req, res) => { // making the endpoint
+    store.getNotes().then((data) => {
+        console.log(data);
+        return res.json(data);
+    }).catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+    })
+
+
+//POST notes

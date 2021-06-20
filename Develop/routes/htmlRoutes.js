a82f0663ee838c2f32d6b66a60821999b5c3ce92
Laura Oberwetter <leoberwetter@gmail.com>
2021-06-19 21:27:54 -0500

    html routes done

---
+++ Develop/routes/htmlRoutes.js
@@ -0,0 +1,20 @@
+const router = require ('express').Router(); // 
+const path = require('path'); // good to use since we are serving static files
+
+//defining the notes page route
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/notes.html')) //send notes.html
+})
+
+//defining the home page route
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'))  //send index.html
+})
+
+//defining the default route
+router.get('*', (req, res) => {
+res.sendFile(path.join(__dirname, '../public/index.html'))  //send index.html
+})
+
+
+module.exports = router;

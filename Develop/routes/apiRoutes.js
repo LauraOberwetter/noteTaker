c4b7f6130dac6a5dcec6eac1ad46dcbaca8410ca
Laura Oberwetter <leoberwetter@gmail.com>
2021-06-19 21:55:31 -0500

    post route started

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,16 +1,31 @@
 const fs = require("fs");
 const router = require('express').Router();
-//const store = require('../db/store'); //use this if making seperate store.js file for data
+const store = require('../db/store'); //use this if making seperate store.js file for data
 
 //GET notes
-router.get('/notes', (req, res) => { // making the endpoint
-    store.getNotes().then((data) => {
-        console.log(data);
-        return res.json(data);
-    }).catch((err) => {
-        console.log(err);
-        res.status(500).json(err);
+router.get("/notes", (req, res) => { // making the endpoint
+  store
+    .getNotes()
+    .then((data) => {
+      console.log(data);
+      return res.json(data);
     })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 //POST notes
+router.post("/notes", (req, res) => { // making the endpoint
+  store
+    .postNotes()
+    .then((data) => {
+      console.log(data);
+      return res.json(data);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});

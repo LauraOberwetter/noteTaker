f09d776c8ec438855074e03079d5486309bf2dfe
Laura Oberwetter <leoberwetter@gmail.com>
2021-06-20 21:14:15 -0500

    debugging note return

--- Develop/db/storedNote.js
+++ Develop/db/storedNote.js
@@ -4,37 +4,40 @@ const writeFile = util.promisify(fs.writeFile); //write data to a file
 const readFile = util.promisify(fs.readFile); //read data from file
 
 class StoredNote {
-    read () {
-        return readFile('db/db.json', 'utf8');
+  read() {
+    return readFile("db/db.json", "utf8");
+  }
+
+  write(data) {
+    return writeFile("db/db.json", JSON.stringify(data));
+  }
+
+  //read, parse, and return entered data
+  getStoredNote() {
+    return this.read().then((data) => {
+      let notes;
+      try {
+        notes = [].concat(JSON.parse(data)); //parse the entered notes, capture within notes variable
+      } catch (err) {
+        notes = []; // return blank array if there is no parsed note
+        console.log("err:" + err);
+      }
+      return notes; //return parsed notes
+    });
+  }
+  //when saved
+  //error here? Cannot destructure property 'title' of 'data' as it is undefined. Isn't data defined here?
+  addNote(data) {
+    const { title, text } = data;
+    console.log(data);
+    if (!title || !text) {
+      throw new Error("Please enter a note");
     }
+    const savedNote = {title, text};
 
-    write (data) {
-        return writeFile('db/db.json', JSON.stringify(data));
-    }
+    return savedNote()
 
-    //read, parse, and return entered data 
-    getStoredNote () {
-        return this.read().then((data) => {
-            let notes;
-            try {
-                notes = [].concat(JSON.parse(data)); //parse the entered notes, capture within notes variable
-              } catch (err) {
-                notes = []; // return blank array if there is no parsed note
-              }
-              return notes; //return parsed notes
-            });
-        }
-    //when saved
-    //error here? Cannot destructure property 'title' of 'data' as it is undefined. Isn't data defined here?
-        addNote(data) {
-        const { title, text } = data;
-    
-        if (!title || !text) {
-            throw new Error("Please enter a note");
-        }
-
-    }
-
+  }
 };
 
 module.exports = new StoredNote();
